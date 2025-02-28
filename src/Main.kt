029177427d19719336c18f721ef2b402f29d8dcb
lorengie <angielojp56@gmail.com>
2025-02-28 00:15:29 -0500

    edificio

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*
 fun main() {
    /* Un edificio tiene un sistema de control de acceso basado en identificaciones.
     Requisitos:
@@ -9,5 +8,29 @@ fun main() {
     •	Si es un empleado, mostrar un mensaje personalizado con su ID.
     •	Si es un visitante, registrar su nombre y motivo de visita antes de ingresa
     */
+
+    val scanner= Scanner(System.`in`)
+    var intentos =0;
+    var codigo:Int
+
+    while (intentos<3){
+        println("Bienvenid@, ingrese su codigo para acceder")
+        codigo=scanner.nextInt()
+        scanner.nextLine()
+        if(codigo in 1000..1999){
+            println("Acceso exitoso, Bienvenido, ID= ${codigo}")
+        }else if (codigo in 2000 ..2999){
+            println("Ingrese su nombre")
+            var nomae= scanner.nextLine()
+            println("Cual es el motivo de su visita?")
+            var motivo = scanner.nextLine()
+            println("Bienvenido, acceso valido ${nomae}, motivo de visita = ${motivo}")
+        }else{
+            intentos++
+            val intentosrest = 3 - intentos
+            println("Codigo invalido, tiene $intentosrest Intentos")
+        }
+    }
+    println("Acceso Bloqueado")
 
 }
