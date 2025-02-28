ee9a8e9a9a8000bb486983fbdfb00ba1707ec3d2
lorengie <angielojp56@gmail.com>
2025-02-27 23:40:52 -0500

    edificio tarea

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   //Ejercicios realizados en la materia desarollo basado en plataformas
+   /* Un edificio tiene un sistema de control de acceso basado en identificaciones.
+    Requisitos:
+    •	Pedir al usuario que ingrese su código de acceso.
+    •	Los códigos válidos son del 1000 al 1999 (empleados) y del 2000 al 2999 (visitantes).
+    •	Si el código es incorrecto 3 veces seguidas, bloquear el acceso.
+    •	Si es un empleado, mostrar un mensaje personalizado con su ID.
+    •	Si es un visitante, registrar su nombre y motivo de visita antes de ingresa
+    */
+
 }
